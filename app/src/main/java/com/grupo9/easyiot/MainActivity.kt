8bf96745ee69e23539f749264e2cd453715ec620
Juan Ignacio Causse <jcausse@itba.edu.ar>
2024-06-13 18:24:20 -0300

    Disabled edgeToEdge and dynamicColor

--- app/src/main/java/com/grupo9/easyiot/MainActivity.kt
+++ app/src/main/java/com/grupo9/easyiot/MainActivity.kt
@@ -17,7 +17,7 @@ import com.grupo9.easyiot.ui.theme.EasyIoTTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        // enableEdgeToEdge()
         setContent {
             EasyIoTTheme {
                 EasyIoTApp()

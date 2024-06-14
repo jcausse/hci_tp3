b0ae695fa91eaaf5c7a75d40995bcbe5a73e524f
Patricklt2 <ptorlaschis@gmail.com>
2024-06-14 19:33:36 -0300

    added device status, modularized

---
+++ app/src/main/java/com/grupo9/easyiot/DeviceCard.kt
@@ -0,0 +1,93 @@
+package com.grupo9.easyiot
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+val kodchasan = FontFamily(
+    Font(R.font.kodchasan_regular, FontWeight.Normal),
+    Font(R.font.kodchasan_bold, FontWeight.Bold),
+)
+
+private val deviceTypeToDrawable = mapOf(
+    "fridge" to R.drawable.fridge,
+    "faucet" to R.drawable.faucet,
+    "door" to R.drawable.door,
+    "blinds" to R.drawable.blinds,
+    "speaker" to R.drawable.speaker,
+    "vacuum" to R.drawable.vacuum,
+    "lamp" to R.drawable.lamp
+    // Add more mappings as needed
+)
+
+// Method to get drawable resource ID based on device type
+fun getDrawableForDeviceType(deviceType: String): Int {
+    return deviceTypeToDrawable[deviceType] ?: R.drawable.file_question
+}
+@Composable
+fun DeviceCard(name: String, statusOn: Boolean, type: String) {
+    androidx.compose.material3.Card(
+        modifier = Modifier
+            .padding(10.dp)
+            .width(160.dp)
+            .height(160.dp),
+        colors = androidx.compose.material3.CardDefaults.cardColors(containerColor = androidx.compose.ui.graphics.Color.White, contentColor = MaterialTheme.colorScheme.primary),
+        elevation = androidx.compose.material3.CardDefaults.cardElevation(4.dp)
+    ) {
+        Column (
+            modifier = Modifier
+                .padding(10.dp)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center, // Center vertically
+            horizontalAlignment = Alignment.CenterHorizontally // Center horizontally
+        ) {
+            Row (
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Absolute.Left
+            ){
+                if(statusOn){
+                    Icon(painter = painterResource(R.drawable.power),contentDescription = "status")
+                } else {
+                    Icon(painter = painterResource(R.drawable.power),contentDescription = "status")
+                }
+            }
+            Row (
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+            }
+            Icon(painter = painterResource( getDrawableForDeviceType(type)),
+                contentDescription = "h",
+                Modifier.size(58.dp),
+                tint = MaterialTheme.colorScheme.primary )
+            TitleDeviceCard(text = name)
+        }
+    }
+}
+@Composable
+fun TitleDeviceCard(text: String) {
+    androidx.compose.material3.Text(
+        modifier = Modifier.padding(5.dp),
+        fontSize = 24.sp,
+        color = MaterialTheme.colorScheme.primary,
+        text = text,
+        fontFamily = kodchasan,
+        fontWeight = FontWeight.Normal
+    )
+}
--- app/src/main/java/com/grupo9/easyiot/Screens/Devices.kt
+++ app/src/main/java/com/grupo9/easyiot/Screens/Devices.kt
@@ -1,29 +1,23 @@
 package com.grupo9.easyiot.Screens
 
-import android.graphics.drawable.Icon
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Info
-import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.grupo9.easyiot.DeviceCard
 import com.grupo9.easyiot.R
 
 
@@ -31,22 +25,6 @@ val kodchasan = FontFamily(
     Font(R.font.kodchasan_regular, FontWeight.Normal),
     Font(R.font.kodchasan_bold, FontWeight.Bold),
 )
-
-private val deviceTypeToDrawable = mapOf(
-    "fridge" to R.drawable.fridge,
-    "faucet" to R.drawable.faucet,
-    "door" to R.drawable.door,
-    "blinds" to R.drawable.blinds,
-    "speaker" to R.drawable.speaker,
-    "vacuum" to R.drawable.vacuum,
-    "lamp" to R.drawable.lamp
-    // Add more mappings as needed
-)
-
-// Method to get drawable resource ID based on device type
-fun getDrawableForDeviceType(deviceType: String): Int {
-    return deviceTypeToDrawable[deviceType] ?: R.drawable.file_question
-}
 
 @Preview(showBackground = true)
 @Composable
@@ -62,34 +40,6 @@ fun DevicesScreen(
     }
 }
 @Composable
-fun DeviceCard(name: String, statusOn: Boolean, type: String) {
-    androidx.compose.material3.Card(
-        modifier = Modifier
-            .padding(10.dp)
-            .width(160.dp)
-            .height(160.dp),
-        colors = androidx.compose.material3.CardDefaults.cardColors(containerColor = androidx.compose.ui.graphics.Color.White, contentColor = androidx.compose.material3.MaterialTheme.colorScheme.primary),
-        elevation = androidx.compose.material3.CardDefaults.cardElevation(4.dp)
-    ) {
-        Column (
-            modifier = Modifier
-                .padding(10.dp)
-                .fillMaxSize(),
-            verticalArrangement = Arrangement.Center, // Center vertically
-            horizontalAlignment = Alignment.CenterHorizontally // Center horizontally
-        ) {
-            androidx.compose.foundation.layout.Row (
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-            }
-            Icon(painter = painterResource(getDrawableForDeviceType(type)), contentDescription = "h")
-            TitleDeviceCard(text = name)
-        }
-    }
-}
-
-@Composable
 fun TitleDevices(text: String) {
     androidx.compose.material3.Text(
         modifier = Modifier.padding(5.dp),
@@ -98,19 +48,6 @@ fun TitleDevices(text: String) {
         text = text,
         fontFamily = kodchasan,
         fontWeight = FontWeight.Bold
-    )
-}
-
-
-@Composable
-fun TitleDeviceCard(text: String) {
-    androidx.compose.material3.Text(
-        modifier = Modifier.padding(5.dp),
-        fontSize = 24.sp,
-        color = androidx.compose.material3.MaterialTheme.colorScheme.primary,
-        text = text,
-        fontFamily = kodchasan,
-        fontWeight = FontWeight.Normal
     )
 }
 

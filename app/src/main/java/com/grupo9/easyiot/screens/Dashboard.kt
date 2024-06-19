2123ad07ab1e5975351428f9c5bc36131a8635e9
Juan Ignacio Causse <jcausse@itba.edu.ar>
2024-06-19 18:48:31 -0300

    WIP on screen/dashboard

--- app/src/main/java/com/grupo9/easyiot/screens/Dashboard.kt
+++ app/src/main/java/com/grupo9/easyiot/screens/Dashboard.kt
@@ -1,2 +1,9 @@
 package com.grupo9.easyiot.screens
 
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun DashboardScreen(dashboardState: DashboardState) {
+    Text(text = "DashBoard screen")
+}
--- app/src/main/java/com/grupo9/easyiot/screens/dashboardViewModel.kt
+++ app/src/main/java/com/grupo9/easyiot/screens/dashboardViewModel.kt
@@ -1,2 +1,21 @@
 package com.grupo9.easyiot.screens
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.grupo9.easyiot.model.device.Devices
+
+class DashboardViewModel : ViewModel(){
+    var dashboardState: DashboardState by mutableStateOf(DashboardState.Loading)
+    private set
+}
+
+sealed interface DashboardState{
+    //success
+    data class Success(val get: Devices) : DashboardState
+    //error
+    data class Error(val message: String) : DashboardState
+    //loading
+    data object Loading : DashboardState
+}

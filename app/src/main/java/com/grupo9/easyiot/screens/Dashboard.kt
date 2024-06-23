95af69cd6ca330cd04d6dcc95ebe0e2f24bc930b
Patricklt2 <ptorlaschis@gmail.com>
2024-06-22 23:08:42 -0300

    added refresh on dashboard

--- app/src/main/java/com/grupo9/easyiot/screens/Dashboard.kt
+++ app/src/main/java/com/grupo9/easyiot/screens/Dashboard.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -14,7 +15,7 @@ import com.grupo9.easyiot.model.device.DeviceResult
 import com.grupo9.easyiot.screens.DevicesState as DevicesState
 
 @Composable
-fun DashboardScreen(recentDevices: SnapshotStateList<String>, devicesState: DevicesState) {
+fun DashboardScreen(recentDevices: SnapshotStateList<String>, devicesViewModel: DevicesViewModel) {
     Column(
         modifier = Modifier.fillMaxSize()
     ) {
@@ -24,12 +25,16 @@ fun DashboardScreen(recentDevices: SnapshotStateList<String>, devicesState: Devi
             verticalArrangement = Arrangement.Center, // Center vertically
             horizontalAlignment = Alignment.CenterHorizontally // Center horizontally
         ) {
-            if (devicesState !is DevicesState.Success){
+            LaunchedEffect(Unit) {
+                devicesViewModel.getDevices()
+            }
+            val devicesState = devicesViewModel.devicesState
+            if (devicesState is DevicesState.Error){
                 Text(text = stringResource(R.string.devices_error_msg))
             }
-            else if (recentDevices.size == 0) {
+             else if ((devicesState is DevicesState.Success && recentDevices.size == 0) || devicesState is DevicesState.Loading) {
                 Text(text = stringResource(R.string.no_recent_devices_message))
-            } else {
+            } else if (devicesState is DevicesState.Success) {
                 val recentDevicesDetails: ArrayList<DeviceResult> = ArrayList()
                 for (device in devicesState.get.result){
                     if (recentDevices.contains(device.id)){
